216b4be61fcc79d95a7044e153020aae5d173c16
nb1017 <fortoon21@naver.com>
2018-12-05 21:37:30 +0900

    'gitignore_added'

--- main.py
+++ main.py
@@ -7,96 +7,121 @@ from utils import *
 from models.VGGNet import VGG
 import torchvision
 import torchvision.transforms as transforms
+import os
+from tensorboardX import SummaryWriter
 
-if __name__=='__main__':
-    def train(epoch):
-        Loss = []
-        print('\nEpoch : %d' % epoch)
+def train(epochs,best_acc=0.):
+    criterion=nn.CrossEntropyLoss()
+    optimizer=optim.Adam(net.parameters(), lr=0.001, betas=[0.5,0.999])
+    print_every=20
+
+    Loss=[]
+
+    for epoch in range(epochs):
+        print('Epoch : %d/%d' % (epoch+1, epochs))
         net.train()
-        train_loss = 0
-        correct = 0
-        total = 0
-        for batch_idx, (inputs, targets) in enumerate(trainloader):
-            inputs, targets = inputs.to(device), targets.to(device)
+        for i, (images, labels) in enumerate(train_loader):
+            images, labels=images.to(device), labels.to(device)
             optimizer.zero_grad()
-            outputs = net(inputs)
-            loss = criterion(outputs, targets)
+            outputs=net(images)
+            loss=criterion(outputs, labels)
+            if (i+1)%print_every==0 :
+                print('Loss : %.5f' % loss.item())
+                writer.add_scalar('clsf/train_loss', loss.item())
+                Loss.append(loss.item())
             loss.backward()
             optimizer.step()
-            Loss.append(loss.item())
 
-            train_loss += loss.item()
-            _, predicted = outputs.max(1)
-            total += targets.size(0)
-            correct += predicted.eq(targets).sum().item()
-            if batch_idx%100==99:
-                print('Loss : {}'.format(loss.item()))
-        torch.save('ckpt.pt',{
-            'net':net.state_dict(),
-            'acc': correct/total,
-            'epoch':epoch
-        })
-
-
-    def test():
-        global best_acc
         net.eval()
-        test_loss = 0
-        correct = 0
-        total = 0
-        with torch.no_grad():
-            for batch_idx, (inputs, targets) in enumerate(testloader):
-                inputs, targets = inputs.to(device), targets.to(device)
-                outputs = net(inputs)
-                loss = criterion(outputs, targets)
+        print('Validation starts!')
+        total=0
+        correct=0
+        for i, (images, labels) in enumerate(valid_loader):
+            images, labels=images.to(device), labels.to(device)
+            outputs=net(images)
+            loss=criterion(outputs,labels)
+            _,predicted=torch.max(outputs.data,1)
+            correct+=torch.sum(predicted==labels.data)
+            total+=labels.size(0)
+            if (i + 1) % print_every == 0:
+                writer.add_scalar('clsf/validation_loss', loss.item())
+                print('Loss : ',loss.item())
 
-                test_loss += loss.item()
-                _, predicted = outputs.max(1)
-                total += targets.size(0)
-                correct += predicted.eq(targets).sum().item()
-            print('accuracy : {}'.format(correct/total))
-
-
-    device = 'cuda:0' if torch.cuda.is_available() else 'cpu'
-
-    best_acc = 0
-    start_epoch = 0
-    print('--> Preparing data..')
-    transform_train = transforms.Compose([
-        transforms.RandomCrop(32, padding=4),
-        transforms.RandomHorizontalFlip(),
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-    ])
-
-    transform_test=transforms.Compose([
-        transforms.ToTensor(),
-        transforms.Normalize((0.4914, 0.4822, 0.4465), (0.2023, 0.1994, 0.2010))
-    ])
-
-    trainset=torchvision.datasets.CIFAR10(root='./data', train=True, transform=transform_train,download=True)
-    trainloader=DataLoader(dataset=trainset, batch_size=16, shuffle=True, num_workers=0)
-
-    testset=torchvision.datasets.CIFAR10(root='./data', train=False, transform=transform_test,download=True)
-    testloader=DataLoader(dataset=testset, batch_size=16,shuffle=False,num_workers=0)
-
-    learning_rate=0.001
-    net=VGG('VGG16')
-    net=net.to(device)
-
-    try :
-        print('>>>> Resuming from checkpoint..')
-        checkpoint=torch.load('./checkpoint/ckpt.pt')
-        net.load_state_dict(checkpoint['net'])
-        best_acc=checkpoint['acc']
-        best_epoch=checkpoint['epoch']
-    except:
-        print('Error: no checkpoint directory found!')
+        acc=correct.item()/total
+        print('Acc : %.4f' % acc)
+        if acc>best_acc:
+            best_acc=acc
+            if not os.path.exists(ckpt_dir):
+                os.mkdir(ckpt_dir)
+                torch.save({
+                    'net' : net.state_dict(),
+                    'acc' : acc},
+                    os.path.join(ckpt_dir,'ckpt.7'))
+            else :
+                torch.save({
+                    'net' : net.state_dict(),
+                    'acc' : acc}, os.path.join(ckpt_dir,'ckpt.7'))
 
 
+def test():
+    print_every=20
     criterion=nn.CrossEntropyLoss()
+    net.eval()
+    print('Test starts!')
+    total = 0
+    correct = 0
+    for i,(images, labels) in enumerate(valid_loader):
+        images, labels = images.to(device), labels.to(device)
+        outputs = net(images)
+        loss = criterion(outputs, labels)
+        _, predicted = torch.max(outputs.data, 1)
+        correct += torch.sum(predicted == labels.data)
+        total += labels.size(0)
+        if (i + 1) % print_every == 0:
+            print('Loss : ', loss.item())
+    acc = correct.item() / total
+    print('Acc : %.4f' % acc)
 
-    optimizer=optim.Adam(net.parameters(), lr=learning_rate)
+if __name__=='__main__':
+    writer=SummaryWriter()
 
-    train(5)
+    train_set = torchvision.datasets.CIFAR10('./data', train=True, transform=transforms.ToTensor(), download=True)
+    mean, std = get_mean_and_std(train_set)
+    train_transform = transforms.Compose([
+        transforms.RandomHorizontalFlip(),
+        transforms.RandomRotation([0.5, 15]),
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std)
+    ])
+
+    test_transform = transforms.Compose([
+        transforms.ToTensor(),
+        transforms.Normalize(mean=mean, std=std)
+    ])
+
+    train_set = torchvision.datasets.CIFAR10('./data', train=True, transform=train_transform, download=True)
+    test_set = torchvision.datasets.CIFAR10('./data', train=False, transform=test_transform, download=True)
+    valid_set = torchvision.datasets.CIFAR10('./data', train=True, transform=transforms.Compose(
+        [transforms.ToTensor(), transforms.Normalize(mean=mean, std=std)]))
+
+    train_loader = torch.utils.data.DataLoader(train_set, shuffle=True, batch_size=32, num_workers=4)
+    test_loader = torch.utils.data.DataLoader(test_set, shuffle=False, batch_size=32, num_workers=4)
+    valid_loader = torch.utils.data.DataLoader(valid_set, shuffle=True, batch_size=32, num_workers=4)
+
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+    ckpt_dir = './ckpt'
+    print('Start ...')
+    net = VGG('VGG16')
+    net = net.to(device)
+    best_acc=0.
+
+    if os.path.exists(os.path.join(ckpt_dir,'ckpt.7')):
+        print('Loading ...')
+        loading=torch.load(os.path.join(ckpt_dir,'ckpt.7'))
+        net=loading['net']
+        best_acc=loading['acc']
+
+    train(1,best_acc)
     test()
+    writer.close()
